2e7ccc592f89e4f5a985eaa8e8cb8b6790b6f3b6
Colin Fcong <cfcong@gmail.com>
2013-11-29 21:25:09 +0000

    added comment on the assigns to better understand what the assigns methods does

--- spec/controllers/trainlines_controller_spec.rb
+++ spec/controllers/trainlines_controller_spec.rb
@@ -8,7 +8,7 @@ describe TrainlinesController do
     it "lists the trainlines" do
       trainline1, trainline2 = FactoryGirl.create(:trainline), FactoryGirl.create(:trainline)
       get :index
-      trainlines = assigns(:trainlines)
+      trainlines = assigns(:trainlines) # @trainlines
       expect(trainlines).to eq([trainline1, trainline2])
       expect(response).to render_template("index")
       expect(response).to be_success # http 200
