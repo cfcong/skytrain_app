b583cbfdafc38f2431b9e6c61c41fcc554793de9
Colin Fcong <cfcong@gmail.com>
2013-12-06 04:22:50 +0000

    tdd testing on payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,24 @@
 class Payment < ActiveRecord::Base
+
+  before_create :set_paid_at
+  after_create :send_receipt_email
+
+  belongs_to :subscription
+
+  validates :subscription_id,
+    presence: true
+
+  validates :amount,
+    presence: true,
+    numericality: true
+
+  validates :paid_at,
+    presence: true
+
+  def set_paid_at
+  end
+
+  def send_receipt_email
+  end
+
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Payment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validation" do
+
+    it "should fail if there is no subscription" do
+      @payment = FactoryGirl.build :payment, subscription: nil
+      expect(@payment).to be_invalid
+      expect(@payment.errors[:subscription].first).to eq(nil)
+    end
+
+    it "should fail if subscription_id does not exist" do
+      @payment = FactoryGirl.build :payment, subscription_id: nil
+      expect(@payment).to be_invalid
+      expect(@payment.errors[:subscription_id].first).to eq("can't be blank")
+    end
+    
+    it "should fail if amount does not exist" do
+      @payment = FactoryGirl.build :payment, amount: nil
+      expect(@payment).to be_invalid
+      expect(@payment.errors[:amount].first).to eq("can't be blank")
+    end  
+  
+    it "should fail if amount is entered incorrect" do
+      @payment = FactoryGirl.build :payment, amount: "hello"
+      expect(@payment).to be_invalid
+      expect(@payment.errors[:amount].first).to eq("is not a number")
+    end
+
+  end
+
 end
