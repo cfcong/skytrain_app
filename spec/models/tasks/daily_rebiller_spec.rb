62d48b2836a2db4e349fb10c7e3d06ada4ba722d
Colin Fcong <cfcong@gmail.com>
2013-12-08 00:34:19 +0000

    finished test on the DailyRebiller class run method

--- spec/models/tasks/daily_rebiller_spec.rb
+++ spec/models/tasks/daily_rebiller_spec.rb
@@ -10,7 +10,17 @@ describe Tasks::DailyRebiller do
       expect(Tasks::SubscriptionCharge).to receive(:new).once.and_return(fake_subscription_charge)
       Tasks::DailyRebiller.new.run
     end
-    pending 'performs a subscription charge for a subscription that was last charged over one month ago'
-    pending 'does not performs a subscription charge for a subscription that was last charged less than one month ago'
+    it 'performs a subscription charge for a subscription that was last charged over one month ago' do
+      FactoryGirl.create :subscription_overide_for_charge
+      fake_subscription_charge = double(:charge! => nil)
+      expect(fake_subscription_charge).to receive(:charge!).once
+      expect(Tasks::SubscriptionCharge).to receive(:new).once.and_return(fake_subscription_charge)
+      Tasks::DailyRebiller.new.run
+    end 
+    it 'does not performs a subscription charge for a subscription that was last charged less than one month ago' do
+      FactoryGirl.create :subscription_charged_just_this_month
+      expect(Tasks::SubscriptionCharge).to_not receive(:new)
+      Tasks::DailyRebiller.new.run
+    end  
   end
 end
